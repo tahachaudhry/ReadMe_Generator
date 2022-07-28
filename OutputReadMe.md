6ff8020c83cea877cf4a1d55419a82aee508f50e
tahachaudhry <taha@nyu.edu>
2022-07-29 01:38:59 +0500

    updated ReadMe

---
+++ OutputReadMe.md
@@ -0,0 +1,32 @@
+# Movie recomendation search engine 
+![badge](https://img.shields.io/badge/license-mpl-2.0-yellow)
+  
+## Description
+  undefined 
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Testing](#testing)
+- [License](#license)
+  
+## Installation
+  undefined
+  
+## Usage
+  If you are bored and want to get movie recomendations to watch  
+  
+## Contributing
+  By intergrating ML algo  
+  
+## Testing
+  N?A  
+## License
+  mpl-2.0
+  
+  
+## Questions
+  If you have any questions, please feel free to contact me on github or via email.
+  Github:tahachaudhry
+  Email:taha@nyu.edu
---
+++ index.js
@@ -0,0 +1,68 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./generateMarkdown');
+const writeFileSync = util.promisify(fs.writeFile);
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'descitption',
+        message: 'What is the description of your project',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage of this project?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How would you like others to contribute to this project?',
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'What are the test instructions?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license do you want?',
+        choices: ["MIT", "mpl-2.0", "osl-3.0" ]
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email and what is the best way to reach you?',
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    writeFileSync(fileName, data)
+    console.log("file written sucessfully")
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(data => {
+        writeToFile("OutputReadMe.md", generateMarkdown(data));
+    })
+}
+
+// Function call to initialize app
+init();
