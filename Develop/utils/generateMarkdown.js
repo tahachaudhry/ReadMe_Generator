b3206df2c4dd7b4377a55bac86f5e68652b519a3
tahachaudhry <taha@nyu.edu>
2022-07-29 01:13:16 +0500

    initial commit

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,36 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+![badge](https://img.shields.io/badge/license-${data.license}-yellow)
+  
+## Description
+  ${data.description} 
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Testing](#testing)
+- [License](#license)
+  
+## Installation
+  ${data.installation}
+  
+## Usage
+  ${data.usage} 
+  
+## Contributing
+  ${data.contributing} 
+  
+## Testing
+  ${data.testing}  
+## License
+  ${data.license}
+  
+  
+## Questions
+  If you have any questions, please feel free to contact me on github or via email.
+  Github:${data.github}
+  Email:${data.email}
 `;
 }
 
